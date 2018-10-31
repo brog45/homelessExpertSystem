63851698c6e037926db2d1d01b029841267812f1
Brian Rogers <brog45@gmail.com>
2018-10-30 23:14:05 -0500

    remove extraneous story dcg code

--- story_dcg_html.pl
+++ story_dcg_html.pl
@@ -13,13 +13,4 @@ step(Step) -->
     { phrase(step_(Step), StepHtml) },
     [ li(StepHtml) ].
 
-step_(pee) --> ['Use the restroom.'].
-step_(wash_hands(_)) --> ['Wash your hands!'].
-step_(dress(For)) --> ['Dress for '], atom(For), ['.'].
-step_(eat) --> ['Om nom nom.'].
-step_(spill) --> ['Oh no! You spilled food on your shirt!'].
-step_(grab(Object)) --> ['Pick up the '], atom(Object), ['.'].
 step_(move(A,B)) --> ['Walk from the '], atom(A), [' to the '], atom(B), ['.'].
-step_(drop(Object)) --> ['Put down the '], atom(Object), ['.'].
-step_(needy_pet(Name, Animal)) --> ['~w the ~w demands love.'-[Name, Animal]].
-step_(love_pet(Name, _Animal)) --> ['You pet ~w for a moment.'-[Name]].
